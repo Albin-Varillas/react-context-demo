a75c35e838cbdd7b4cf9899f6de7ca17dd122432
Albin Varillas <81118732+Albin-Varillas@users.noreply.github.com>
2023-01-12 11:27:40 +0100

    use context

--- src/App.jsx
+++ src/App.jsx
@@ -1,34 +1,54 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import './App.css'
+import { createContext, useContext, useState } from "react"
+import "./App.css"
+
+const SnusContext = createContext(null)
 
 function App() {
-  const [count, setCount] = useState(0)
+    const [antal, setAntal] = useState(4)
 
-  return (
-    <div className="App">
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src="/vite.svg" className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://reactjs.org" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </div>
-  )
+    function decrement() {
+        setAntal(antal - 1)
+    }
+
+    return (
+        <SnusContext.Provider
+            value={{
+                snusHemma: "general",
+                antal: antal,
+                setAntal: setAntal,
+                decrement: decrement,
+            }}
+        >
+            <div className="App">
+                hej
+                <SnusDosa />
+            </div>
+        </SnusContext.Provider>
+    )
+}
+
+function SnusDosa() {
+    const { snusHemma, antal, setAntal, decrement } = useContext(SnusContext)
+
+    return (
+        <div>
+            Jag har {snusHemma} hemma och har {antal} stycken
+            <button
+                onClick={() => {
+                    setAntal(antal + 1)
+                }}
+            >
+                öka antal
+            </button>
+            <button
+                onClick={() => {
+                    decrement()
+                }}
+            >
+                minska antal
+            </button>
+        </div>
+    )
 }
 
 export default App
